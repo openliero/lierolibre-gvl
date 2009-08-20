dd0ecf0cfdc640322b0b016d2df00959998a399b
Erik Lindroos <gliptic@gmail.com>
2009-08-20 17:37:33 +0200

    Forgot adding a few files

---
+++ gvl_test/support/bits.cpp
@@ -0,0 +1,73 @@
+#include <gvl/tut/tut.hpp>
+
+#include <gvl/math/cmwc.hpp>
+#include <gvl/support/bits.hpp>
+
+namespace tut
+{
+
+struct bits_data
+{
+	
+};
+
+typedef test_group<bits_data> factory;
+typedef factory::object object;
+
+}
+
+namespace
+{
+	tut::factory tf("bit operations");
+} // namespace
+
+namespace tut
+{
+
+int reference_trailing_zeroes(uint32_t v)
+{
+	int c = 0;
+	if(!v) return 0;
+	while(!((v >> c) & 1))
+	{
+		++c;
+	}
+	return c;
+}
+
+int reference_bottom_bit(uint32_t v)
+{
+	if(!v) return -1;
+	return reference_trailing_zeroes(v);
+}
+
+int reference_log2(uint32_t v)
+{
+	int c = 0;
+	while((v >>= 1))
+		++c;
+	return c;
+}
+
+int reference_top_bit(uint32_t v)
+{
+	if(!v) return -1;
+	return reference_log2(v);
+}
+
+template<>
+template<>
+void object::test<1>()
+{
+	gvl::mwc rand;
+	for(int i = 0; i < 1000000; ++i)
+	{
+		uint32_t x = rand();
+		ensure("trailing_zeroes", reference_trailing_zeroes(x) == gvl::trailing_zeroes(x));
+		ensure("bottom_bit", reference_bottom_bit(x) == gvl::bottom_bit(x));
+		ensure("log2", reference_log2(x) == gvl::log2(x));
+		ensure("top_bit", reference_top_bit(x) == gvl::top_bit(x));
+	}
+}
+
+}
---
+++ math/random.hpp
@@ -0,0 +1,37 @@
+#ifndef UUID_F29926F3240844A09DCFB9B1828C7DC8
+#define UUID_F29926F3240844A09DCFB9B1828C7DC8
+
+namespace gvl
+{
+
+template<typename DerivedT, typename ValueT>
+struct prng_common
+{
+	typedef ValueT value_type;
+	
+	DerivedT& derived()
+	{ return *static_cast<DerivedT*>(this); }
+	
+	double get_double(double max)
+	{
+		uint32_t v = derived()();
+		return v * max / 4294967296.0;
+	}
+	
+	uint32_t operator()(uint32_t max)
+	{
+		uint64_t v = derived()();
+		v *= max;
+		return uint32_t(v >> 32);
+	}
+	
+	uint32_t operator()(uint32_t min, uint32_t max)
+	{
+		return derived()(max - min) + min;
+	}
+};
+
+} // namespace gvl
+
+#endif // UUID_F29926F3240844A09DCFB9B1828C7DC8
+

7594ea7e8196f56928a6db69960aba11c3ba2980
Erik Lindroos <gliptic@gmail.com>
2010-03-27 16:11:18 +0100

    Forgot to add new bitstreams

---
+++ io/bitstream.hpp
@@ -0,0 +1,216 @@
+#ifndef UUID_4C44D1D95C5C40B194BE5294E8B984DE
+#define UUID_4C44D1D95C5C40B194BE5294E8B984DE
+
+#include "../support/cstdint.hpp"
+#include "../support/bits.hpp"
+#include <algorithm>
+#include <stdexcept>
+#include <cstddef>
+#include <climits>
+
+namespace gvl
+{
+
+#define GVL_BITMASK(b) (~(unsigned int)0) >> (32-(b))
+
+template<typename DerivedT, int BufBytes = 4>
+struct basic_ibitstream
+{
+	typedef uint8_t byte;
+	typedef unsigned int bitbuf_t; // We hope unsigned int is the fastest type to deal with
+
+	static int const in_buf_bits = BufBytes * 8;
+	static unsigned int const in_buf_mask = GVL_BITMASK(in_buf_bits);
+
+	basic_ibitstream()
+	: in_bits(0)
+	, in_bit_count(in_buf_bits)
+	{
+	}
+		
+	template<typename T2>
+	basic_ibitstream(basic_ibitstream<T2, BufBytes> const& b)
+	: in_bits(b.in_bits)
+	, in_bit_count(b.in_bit_count)
+	{
+		
+	}
+	
+	DerivedT& derived()
+	{
+		return *static_cast<DerivedT*>(this);
+	}
+	
+	// Number of calls to get_byte that will be done
+	// if 'bits' bits are read from the stream after this
+	// point.
+	std::size_t in_bytes_required(std::size_t bits) const
+	{
+		return in_chunks_required(bits) * BufBytes;
+	}
+	 
+	std::size_t in_chunks_required(std::size_t bits) const
+	{
+		return ((bits + in_bit_count - 1) / in_buf_bits);
+	}
+
+	uint32_t in_bits_in_buffer() const
+	{ return in_buf_bits - in_bit_count; }
+
+	unsigned int in_bits_as_uint() const
+	{ return gvl::shr_1_32(in_bits, in_bit_count); }
+
+	template<int C>
+	bitbuf_t get_chunk();
+
+	template<int C>
+	void ignore_chunks(int c);
+
+	bitbuf_t get_buf()
+	{ return derived().get_chunk<BufBytes>(); }
+
+	void ignore_bufs(int c)
+	{ derived().ignore_chunks<BufBytes>(c); }
+	
+	void swap(basic_ibitstream& b)
+	{
+		std::swap(in_bit_count, b.in_bit_count);
+		std::swap(in_bits, b.in_bits);
+	}
+
+	void ignore(uint32_t bits);
+	uint32_t get();
+	uint32_t get_uint(uint32_t bits);
+	void get_block(void* ptr_, size_t len);
+	uint32_t get_lim(uint32_t low, uint32_t high);
+	void resetg();
+	
+	void get_debug_mark();
+	
+	bitbuf_t in_bits;
+	uint32_t in_bit_count;
+};
+
+template<typename DerivedT, int BufBytes = 4>
+struct basic_obitstream
+{
+	typedef uint8_t byte;
+	typedef unsigned int bitbuf_t; // We hope unsigned int is the fastest type to deal with
+
+	static int const out_buf_bits = BufBytes * 8;
+	static unsigned int const out_buf_mask = GVL_BITMASK(out_buf_bits);
+	
+	basic_obitstream()
+	: out_bits(0)
+	, out_bit_count(0)
+	{
+	}
+		
+	template<typename T2>
+	basic_obitstream(basic_obitstream<T2, BufBytes> const& b)
+	: out_bits(b.out_bits)
+	, out_bit_count(b.out_bit_count)
+	{
+		
+	}
+	
+	DerivedT& derived()
+	{
+		return *static_cast<DerivedT*>(this);
+	}
+	
+	
+	// Bits written if 'bytes' calls to put_byte were made
+	// after an initial state of out_bit_count == 0.
+	std::size_t out_bits_written(std::size_t bytes) const
+	{ return bytes * 8 + out_bit_count;	}
+
+	template<int C>
+	void put_chunk(bitbuf_t);
+
+	void put_buf(bitbuf_t v)
+	{ return derived().put_chunk<BufBytes>(v); }
+
+	uint32_t out_bits_in_buffer() const
+	{ return out_bit_count; }
+
+	uint32_t out_bits_bufferable() const
+	{ return out_buf_bits - out_bit_count; }
+
+	unsigned int out_bits_as_uint() const
+	{ return out_bits; }
+
+	void swap(basic_obitstream& b)
+	{
+		std::swap(out_bit_count, b.out_bit_count);
+		std::swap(out_bits, b.out_bits);
+	}
+
+	void put(uint32_t bit);
+	void put_uint(uint32_t i, uint32_t bits);
+	void put_block(void const* ptr_, std::size_t len);
+
+	template<typename T, int BufBytes2>
+	void put(basic_ibitstream<T, BufBytes2>& src, int bits);
+		
+	void resetp();
+
+	// Finish and align to the buffer size. This will
+	// call put_chunk<BufBytes> at most once.
+	void finish();
+
+	// Finish with a different byte alignment. This
+	// will call put_chunk<C> at most [32/(8*C)] times.
+	template<int ByteAlignment>
+	void finish_aligned();
+	
+	void put_debug_mark();
+	
+	bitbuf_t out_bits;
+	uint32_t out_bit_count;
+};
+
+// Dummy obitstream to allow normalization of interface of bitstreams.
+// Throws on all output operations.
+struct dummy_obitstream
+{
+	void not_supported() const
+	{ throw std::runtime_error("Not supported"); }
+	
+	void put(uint32_t) { not_supported(); }
+	void put_uint(uint32_t, uint32_t) { not_supported(); }
+	void put_block(void const*, std::size_t) { not_supported(); }
+
+	void resetp() { not_supported(); }
+	void finish() { not_supported(); }
+	void clear() { not_supported(); }
+
+	template<int C>
+	void finish_aligned() { not_supported(); }
+	
+	void put_debug_mark() { not_supported(); }
+};
+
+// Dummy ibitstream to allow normalization of interface of bitstreams.
+// Throws on all input operations.
+struct dummy_ibitstream
+{
+	void not_supported() const
+	{ throw std::runtime_error("Not supported"); }
+	
+	std::size_t in_bytes_required(std::size_t bits) const { not_supported(); return 0; }
+	
+	void     ignore(uint32_t bits) { not_supported(); }
+	uint32_t get() { not_supported(); return 0; }
+	uint32_t get_uint(uint32_t bits) { not_supported(); return 0; }
+	void     get_block(void* ptr_, size_t len) { not_supported(); }
+	void     resetg() { not_supported(); }
+	
+	void get_debug_mark() { not_supported(); }
+};
+
+} // namespace gvl
+
+#include "bitstream_templ.hpp"
+
+#endif // UUID_4C44D1D95C5C40B194BE5294E8B984DE
---
+++ io/bitstream_templ.hpp
@@ -0,0 +1,366 @@
+#include "bitstream.hpp"
+#include "../support/debug.hpp"
+#include "../support/platform.hpp"
+#include <cstdio>
+
+#if GVL_MSVCPP
+// Disable "shift count negative or too big, undefined behavior".
+// We make sure such cases are never reached.
+#pragma warning(push)
+#pragma warning(disable: 4293)
+#endif
+
+namespace gvl
+{
+
+template<typename DerivedT, int BufBytes>
+void basic_obitstream<DerivedT, BufBytes>::put(uint32_t bit)
+{
+	uint32_t count = out_bit_count;
+	unsigned int bits = out_bits | (bit << count);
+	++count;
+
+	if(count == out_buf_bits)
+	{
+		put_buf(bits);
+		out_bits = 0;
+		out_bit_count = 0;
+	}
+	else
+	{
+		out_bits = bits;
+		out_bit_count = count;
+	}
+}
+
+template<typename DerivedT, int BufBytes>
+void basic_obitstream<DerivedT, BufBytes>::put_uint(uint32_t i, uint32_t bits)
+{
+	// Bits must be at most 32
+	sassert(bits <= 32);
+	sassert(bits > 0);
+	passert((i & gvl::lsb_mask(bits)) == i, "unwritten bits must be 0");
+	
+	uint32_t written = out_bit_count;
+	uint32_t buf = out_bits | (i << written);
+
+	written += bits;
+
+	if(written >= out_buf_bits)
+	{
+		put_buf(buf & out_buf_mask);
+		written -= out_buf_bits;
+		int consumed_bits = (bits - written); // [1, 32]
+		buf = gvl::shr_1_32(i, consumed_bits);
+
+		while(out_buf_bits < 32 && written >= out_buf_bits)
+		{
+			put_buf(i & out_buf_mask);
+			written -= out_buf_bits;
+			buf = (buf >> out_buf_bits);
+		}
+	}
+
+	out_bits = buf;
+	out_bit_count = written;
+}
+
+template<typename DerivedT, int BufBytes>
+void basic_obitstream<DerivedT, BufBytes>::put_block(void const* ptr_, size_t len)
+{
+	uint8_t const* ptr = static_cast<uint8_t const*>(ptr_);
+	uint8_t const* end;
+	if(len == 0)
+		return;
+	end = ptr + len;
+
+	for(; ptr != end; ++ptr)
+	{
+		put_uint(*ptr, 8);
+	}
+}
+
+template<typename DerivedT, int BufBytes>
+template<typename T, int BufBytes2>
+void basic_obitstream<DerivedT, BufBytes>::put(basic_ibitstream<T, BufBytes2>& src_, int bits)
+{
+	T& src = src_.derived();
+
+	// TODO: Faster case when both streams are aligned to eachother, i.e.
+	// (BufBytes == BufBytes2 && out_bit_count == src.in_bit_count).
+	while(bits > 32)
+	{
+		put_uint(src.get_uint(32), 32);
+		bits -= 32;
+	}
+
+	put_uint(src.get_uint(bits), bits);
+}
+
+// NOTE: Untested
+// Integer in [0, count)
+template<typename BitWriter>
+void put_trunc(BitWriter& writer, uint32_t v, uint32_t count)
+{
+	uint32_t bits = gvl::ceil_log2(count);
+	uint32_t p2 = (1 << bits);
+	uint32_t b = count - p2;
+	if(v < p2 - b)
+		writer.put_uint(v, bits);
+	else
+		writer.put_uint(v + b, bits + 1);
+}
+
+// NOTE: Untested
+template<typename BitWriter>
+void put_unary(BitWriter& writer, uint32_t v)
+{
+	for(; v >= 32; v -= 32)
+		writer.put_uint(0, 32);
+	writer.put_uint(1 << v, v + 1);
+}
+
+// NOTE: Untested
+template<typename BitWriter>
+void put_golomb(BitWriter& writer, uint32_t v, uint32_t m)
+{
+	uint32_t quot = v / m;
+	uint32_t rem = v % m;
+	
+	put_unary(writer, quot);
+	put_trunc(writer, rem, m);
+}
+
+// NOTE: Untested
+template<typename BitWriter>
+void put_rice(BitWriter& writer, uint32_t v, uint32_t shift)
+{
+	sassert(shift > 0);
+	uint32_t quot = v >> shift;
+	uint32_t rem = v & gvl::lsb_mask(shift);
+	
+	put_unary(writer, quot);
+	writer.put_uint(rem, shift);
+}
+
+template<typename DerivedT, int BufBytes>
+void basic_obitstream<DerivedT, BufBytes>::put_debug_mark()
+{
+	put_uint((1<<15)|1, 16);
+}
+
+
+template<typename DerivedT, int BufBytes>
+void basic_ibitstream<DerivedT, BufBytes>::ignore(uint32_t bits)
+{
+	uint32_t cursor = in_bit_count;
+	cursor += bits;
+	
+	uint32_t bufs_to_read = cursor / in_buf_bits;
+	
+	if(bufs_to_read >= 1)
+	{
+		derived().ignore_bufs(bufs_to_read - 1);
+		in_bits = get_buf();
+	}
+	
+	in_bit_count = (cursor % in_buf_bits);
+}
+
+template<typename DerivedT, int BufBytes>
+uint32_t basic_ibitstream<DerivedT, BufBytes>::get()
+{
+	if(in_bit_count == in_buf_bits)
+	{
+		in_bits = get_buf();
+		in_bit_count = 0;
+	}
+	
+	return (in_bits >> in_bit_count++) & 1;
+}
+
+template<typename DerivedT, int BufBytes>
+uint32_t basic_ibitstream<DerivedT, BufBytes>::get_uint(uint32_t bits)
+{
+	// Bits must be at most 32
+	sassert(bits <= 32);
+	sassert(bits > 0);
+	
+	uint32_t read = in_bit_count;
+	bitbuf_t buf = in_bits;
+
+	uint32_t v;
+	uint32_t new_read = read + bits;
+
+	if(new_read <= in_buf_bits)
+	{
+		// read in [2, 31]
+		in_bit_count = new_read;
+		v = buf >> read;
+	}
+	else
+	{
+		// read in [1, 32]
+		v = gvl::shr_1_32(buf, read);
+
+		buf = get_buf();
+		uint32_t written = in_buf_bits - read; // Bits written to v
+		v |= buf << written;
+		written += in_buf_bits;
+		
+		while(in_buf_bits < 32 && written < bits)
+		{
+			buf = get_buf();
+			written += in_buf_bits;
+			v |= buf << written;
+		}
+
+		in_bit_count = in_buf_bits - (written - bits); // Didn't use any surplus bits
+		in_bits = buf;
+	}
+	
+	return v & gvl::lsb_mask(bits);
+}
+
+template<typename DerivedT, int BufBytes>
+void basic_ibitstream<DerivedT, BufBytes>::get_block(void* ptr_, size_t len)
+{
+	if(len == 0)
+		return;
+		
+	uint8_t* ptr = static_cast<uint8_t*>(ptr_);	
+	uint8_t* end = ptr + len;
+	
+	for(; ptr != end; ++ptr)
+		*ptr = get_uint(8);
+}
+
+template<typename BitReader>
+uint32_t get_trunc(BitReader& reader, uint32_t count)
+{
+	uint32_t bits = gvl::ceil_log2(count);
+	uint32_t p2 = (1 << bits);
+	uint32_t b = count - p2;
+	uint32_t v = reader.get_uint(bits);
+	if(v < p2 - b)
+		return v;
+	else
+		return ((v << 1) + get()) - b;
+}
+
+// NOTE: Untested
+template<typename BitReader>
+uint32_t get_unary(BitReader& reader)
+{
+	uint32_t v = 0;
+	for(; !reader.get(); ++v)
+		/* Nothing */;
+	return v;
+}
+
+// NOTE: Untested
+template<typename BitReader>
+uint32_t get_golomb(BitReader& reader, uint32_t m)
+{
+	uint32_t quot = get_unary(reader);
+	uint32_t rem = get_trunc(reader, m);
+	
+	return quot * m + rem;
+}
+
+// NOTE: Untested
+template<typename BitReader>
+uint32_t get_rice(BitReader& reader, uint32_t shift)
+{
+	sassert(shift > 0);
+	
+	uint32_t quot = reader.get_unary();
+	uint32_t rem = reader.get_uint(shift);
+	
+	return (quot << shift) + rem;
+}
+
+
+template<typename DerivedT, int BufBytes>
+void basic_ibitstream<DerivedT, BufBytes>::get_debug_mark()
+{
+	uint32_t m = get_uint(16);
+	passert(m == ((1<<15)|1), "Debug mark not found");
+}
+
+template<typename DerivedT, int BufBytes>
+void basic_ibitstream<DerivedT, BufBytes>::resetg()
+{
+	in_bit_count = in_buf_bits;
+	in_bits = 0;
+}
+
+template<typename DerivedT, int BufBytes>
+void basic_obitstream<DerivedT, BufBytes>::resetp()
+{
+	out_bit_count = 0;
+	out_bits = 0;
+}
+
+template<typename DerivedT, int BufBytes>
+void basic_obitstream<DerivedT, BufBytes>::finish()
+{
+	if(out_bit_count > 0)
+	{
+		put_buf(out_bits);
+		out_bits = 0;
+		out_bit_count = 0;
+	}
+}
+
+template<typename DerivedT, int BufBytes>
+template<int ByteAlignment>
+void basic_obitstream<DerivedT, BufBytes>::finish_aligned()
+{
+	int const bit_alignment = ByteAlignment * 8;
+	if(out_bit_count > 0)
+	{
+		while(true)
+		{
+			put_chunk<C>(out_bits & GVL_BITMASK(bit_alignment));
+			if(out_bit_count <= bit_alignment)
+				break;
+			out_bit_count -= bit_alignment;
+			out_bits >>= bit_alignment;
+		}
+		
+		out_bits = 0;
+		out_bit_count = 0;
+	}
+}
+
+template<typename DerivedT, int BufBytes>
+template<int C>
+unsigned int basic_ibitstream<DerivedT, BufBytes>::get_chunk()
+{
+	bitbuf_t v = derived().get_byte();
+	for(int i = 1; i < C; ++i)
+		v = (v << 8) | derived().get_byte();
+	return v;
+}
+
+template<typename DerivedT, int BufBytes>
+template<int C>
+void basic_ibitstream<DerivedT, BufBytes>::ignore_chunks(int c)
+{
+	derived().ignore_bytes(c * C);
+}
+
+template<typename DerivedT, int BufBytes>
+template<int C>
+void basic_obitstream<DerivedT, BufBytes>::put_chunk(bitbuf_t v)
+{
+	for(int i = C - 1; i >= 0; --i)
+		derived().put_byte(v >> (i*8));
+}
+
+} // namespace gvl
+
+#if GVL_MSCVPP
+#pragma warning(pop)
+#endif

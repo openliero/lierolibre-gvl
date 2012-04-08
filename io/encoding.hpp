5f3844c82cc3ef638406c175f94043f521553841
Martin Erik Werner <martinerikwerner@gmail.com>
2012-04-08 11:47:05 +0200

    io/encoding: fix crlf and whitespace

--- io/encoding.hpp
+++ io/encoding.hpp
@@ -19,7 +19,7 @@ struct basic_text_writer
 {
 	DerivedT& derived()
 	{ return *static_cast<DerivedT*>(this); }
-	
+
 	DerivedT const& derived() const
 	{ return *static_cast<DerivedT const*>(this); }
 };
@@ -35,39 +35,39 @@ struct basic_text_writer
 struct octet_stream_reader : gvl::shared
 {
 	typedef bucket::size_type size_type;
-	
+
 	octet_stream_reader(shared_ptr<stream> source_init)
 	: cur_(0)
 	, end_(0)
 	, source_(source_init)
 	{
 	}
-	
+
 	octet_stream_reader()
 	: cur_(0)
 	, end_(0)
 	, source_()
 	{
 	}
-	
+
 /*
 	// Different naming to avoid infinite recursion if
 	// not defined in DerivedT.
 	bucket_source* get_source()
 	{ return derived()->source(); }
 	*/
-	
+
 	/*
 	shared_ptr<stream> get_source()
 	{ return source_; }*/
-	
+
 	uint8_t get()
 	{
 		// We keep this function small to encourage
 		// inlining
 		return (cur_ != end_) ? (*cur_++) : underflow_get_();
 	}
-	
+
 	void get(uint8_t* dest, std::size_t len)
 	{
 		while(true)
@@ -101,7 +101,7 @@ struct octet_stream_reader : gvl::shared
 		}
 
 	}
-	
+
 	stream::read_status try_get(uint8_t& ret)
 	{
 		if(cur_ != end_)
@@ -114,7 +114,7 @@ struct octet_stream_reader : gvl::shared
 			return underflow_get_(ret);
 		}
 	}
-	
+
 	std::size_t try_get(uint8_t* dest, std::size_t len)
 	{
 		// TODO: Can optimize this
@@ -187,27 +187,27 @@ struct octet_stream_reader : gvl::shared
 
 		return s;
 	}
-		
+
 	bool at_eos()
 	{
 		if(cur_ != end_)
 			return false;
-			
+
 		stream::read_status status = next_bucket_();
 		if(status == stream::read_eos)
 			return true;
-	
-		return false; // TODO: Does erroring count as eos?	
+
+		return false; // TODO: Does erroring count as eos?
 	}
-	
+
 	// TODO: A get that returns a special value for EOF
 
 	// TODO: This returned an auto_read_result before
 	stream::read_result get_bucket(size_type amount = 0);
-	
+
 	// Non-blocking
 	stream::read_result try_get_bucket(size_type amount = 0);
-	
+
 	shared_ptr<stream> detach();
 
 	stream& source()
@@ -215,12 +215,12 @@ struct octet_stream_reader : gvl::shared
 
 	bool has_source() const
 	{ return source_.get() != 0; }
-	
+
 	void attach(shared_ptr<stream> source_new)
 	{
 		if(source_)
 			throw stream_error("A source is already attached to this octet_stream_reader");
-		
+
 		source_ = source_new;
 	}
 
@@ -230,39 +230,39 @@ struct octet_stream_reader : gvl::shared
 		str->seekg(pos);
 		attach(str);
 	}
-	
+
 	/// Amount of data left in the first bucket
 	std::size_t first_left() const { return end_ - cur_; }
-	
+
 private:
-	
+
 	uint8_t underflow_get_();
 	stream::read_status underflow_get_(uint8_t& ret);
-	
+
 	/// Discards the current first bucket (if any) and tries to read
 	/// a bucket if necessary.
 	/// May throw.
 	/// Precondition: cur_ == end_
 	stream::read_status next_bucket_(uint32_t amount = 0);
-	
+
 	void check_source()
 	{
 		if(!source_)
 			throw stream_read_error(stream::read_error, "No source assigned to octet_stream_reader");
 	}
 
-	
-		
+
+
 	// May throw
 	stream::read_result read_bucket_and_return_(size_type amount);
-	
+
 	// May throw
 	stream::read_result try_read_bucket_and_return_(size_type amount)
 	{
 		check_source();
 		return source_->read(amount);
 	}
-		
+
 	/// Apply changes to first bucket
 	void correct_first_bucket_()
 	{
@@ -272,18 +272,18 @@ private:
 			first_->cut_front(old_size - first_left());
 		}
 	}
-	
+
 	void set_first_bucket_(bucket* b)
 	{
 		//passert(!first_.get(), "Still a bucket in first_");
 		size_type s = b->size();
-		
+
 		first_.reset(b);
 		// New first bucket, update cur_ and end_
 		cur_ = b->get_ptr();
 		end_ = cur_ + s;
 	}
-	
+
 	uint8_t const* cur_; // Pointer into first_
 	uint8_t const* end_; // End of data in first_
 	std::auto_ptr<bucket> first_;
@@ -301,8 +301,8 @@ struct octet_stream_writer
 		default_initial_bucket_size = 512,
 		default_max_bucket_size = 32768
 	};
-	
-	
+
+
 	octet_stream_writer(shared_ptr<stream> sink)
 	: sink_()
 	, cur_(0)
@@ -314,7 +314,7 @@ struct octet_stream_writer
 	{
 		attach(sink);
 	}
-	
+
 	octet_stream_writer()
 	: sink_()
 	, cur_(0)
@@ -325,13 +325,13 @@ struct octet_stream_writer
 	, max_bucket_size(default_max_bucket_size)
 	{
 	}
-	
+
 	~octet_stream_writer()
 	{
 		if(sink_)
 			flush();
 	}
-	
+
 	stream::write_status flush(bucket_size new_buffer_size = 0);
 	stream::write_status weak_flush(bucket_size new_buffer_size = 0);
 	//stream::write_status partial_flush();
@@ -342,7 +342,7 @@ struct octet_stream_writer
 		// inlining of the common case
 		return (cur_ != end_) ? (*cur_++ = b, stream::write_ok) : overflow_put_(b);
 	}
-	
+
 	stream::write_status put(uint8_t const* p, std::size_t len)
 	{
 		// We keep this function small to encourage
@@ -361,7 +361,7 @@ struct octet_stream_writer
 				}
 				while(len--)
 					*cur_++ = *p++;
-					
+
 				return stream::write_ok;
 			}
 #endif
@@ -374,7 +374,7 @@ struct octet_stream_writer
 			return overflow_put_(p, len);
 		}
 	}
-	
+
 	stream::write_status put(uint32_t const* p, std::size_t len)
 	{
 		stream::write_status res = stream::write_ok;
@@ -382,21 +382,21 @@ struct octet_stream_writer
 		{
 			res = stream::combine_write_status(res, put(*p++));
 		}
-		return res;	
+		return res;
 	}
-		
+
 	stream::write_status put_bucket(bucket* buf);
-	
+
 	shared_ptr<stream> detach()
 	{
 		if(has_sink())
 		{
 			flush_buffer();
-		
+
 			// Buffer any remaining buckets
 			// partial_flush already does this: sink_->write_buffered(mem_buckets_);
 		}
-		
+
 		return sink_.release();
 	}
 
@@ -405,7 +405,7 @@ struct octet_stream_writer
 
 	bool has_sink() const
 	{ return sink_.get() != 0; }
-	
+
 	void attach(shared_ptr<stream> new_sink)
 	{
 		if(sink_)
@@ -415,13 +415,13 @@ struct octet_stream_writer
 		buffer_.reset(bucket_data_mem::create(cap_, 0));
 		read_in_buffer_();
 	}
-	
+
 	void check_sink()
 	{
 		if(!sink_)
 			throw stream_write_error(stream::write_error, "No sink assigned to octet_stream_writer");
 	}
-	
+
 	void swap(octet_stream_writer& b)
 	{
 		gvl::shared::swap(b);
@@ -429,7 +429,7 @@ struct octet_stream_writer
 		std::swap(cur_, b.cur_);
 		std::swap(end_, b.end_);
 		std::swap(cap_, b.cap_);
-		
+
 		{ // auto_ptr doesn't have swap, so we need to do this
 			std::auto_ptr<bucket_data_mem> tmp = buffer_;
 			buffer_ = b.buffer_;
@@ -437,41 +437,41 @@ struct octet_stream_writer
 		}
 		std::swap(estimated_needed_buffer_size_, b.estimated_needed_buffer_size_);
 	}
-	
+
 	void reserve(std::size_t size)
 	{
 		ensure_cap_(size);
 	}
-	
+
 	// Make the growth of the current bucket unlimited
 	void set_unlimited_bucket()
 	{
 		max_bucket_size = std::size_t(0) - 1;
 	}
-		
+
 private:
 	stream::write_status flush_buffer(bucket_size new_buffer_size = 0);
-	
+
 	std::size_t buffer_size_()
 	{
 		return (cur_ - buffer_->data);
 	}
-	
+
 	void correct_buffer_()
 	{
 		buffer_->size_ = buffer_size_();
 	}
-	
+
 	void read_in_buffer_()
 	{
 		cur_ = buffer_->data + buffer_->size_;
 		end_ = buffer_->data + cap_;
 	}
-	
+
 	stream::write_status overflow_put_(uint8_t b);
-	
+
 	stream::write_status overflow_put_(uint8_t const* p, std::size_t len);
-	
+
 	void ensure_cap_(std::size_t s);
 
 	//bucket_size size_;
@@ -493,16 +493,16 @@ struct raw_ansi_format_writer : basic_text_writer<raw_ansi_format_writer<Writer>
 	: w(w_init)
 	{
 	}
-	
+
 	void flush()
 	{ w.flush(); }
-	
+
 	void put(uint32_t c)
 	{ w.put((uint8_t)c); }
-	
+
 	void put(uint8_t const* b, std::size_t length)
 	{ w.put(b, length); }
-			
+
 	Writer& w;
 };*/
 
@@ -579,16 +579,16 @@ struct cell : basic_text_writer<cell>
 	{
 		left, center, right
 	};
-	
+
 	cell(int width_init, placement text_placement_init)
 	: text_placement(text_placement_init)
 	, width(width_init)
 	{
 	}
-	
+
 	void put(uint32_t x)
 	{ buffer.push_back(x); }
-	
+
 	void put(uint8_t const* p, std::size_t len)
 	{
 		for(std::size_t i = 0; i < len; ++i)
@@ -596,7 +596,7 @@ struct cell : basic_text_writer<cell>
 			buffer.push_back(p[i]);
 		}
 	}
-	
+
 	void put(uint32_t const* p, std::size_t len)
 	{
 		for(std::size_t i = 0; i < len; ++i)
@@ -604,7 +604,7 @@ struct cell : basic_text_writer<cell>
 			buffer.push_back(p[i]);
 		}
 	}
-	
+
 	std::vector<uint32_t> buffer;
 	placement text_placement;
 	int width;
